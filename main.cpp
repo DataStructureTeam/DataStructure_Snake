ef0fbdc65602ae373caad3559677de64748666f4
xikuun <2096458157@qq.com>
2022-12-08 01:12:11 +0800

    课前准备
    第一次合作加油

--- main.cpp
+++ main.cpp
@@ -5,3 +5,4 @@ int main(){
     cout<<"Hello World!"<<endl;
     return 0;
 }
+//https://blog.csdn.net/Brenda_Bestow/article/details/123003386?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_baidulandingword~default-0-123003386-blog-125513206.pc_relevant_landingrelevant&spm=1001.2101.3001.4242.1&utm_relevant_index=3
