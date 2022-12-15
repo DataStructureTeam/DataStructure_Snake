ab61dd47aa12082ef07aea3c2666fb5f49056edb
xikuun <2096458157@qq.com>
2022-12-15 09:48:24 +0800

    lxk_12_15修改

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
-#include<iostream>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-int main(){
-    cout<<"Hello World!"<<endl;
+
+int main() {
+    cout << "DataStructure_Snake" << endl;
     return 0;
 }
+/*李锡堃的小组任务3:
+ * 让蛇动起来:
+ *      1.蛇的移动
+ *      2.蛇的方向
+ *      3.控制方向
+ *      4.调整（倍率，边界等）*/
